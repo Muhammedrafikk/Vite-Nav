9ee83e950dfa9e559b0fcc9d8b3c46a3e7b54871
Muhammedrafikk <muhammedrafi6622@gmail.com>
2025-04-15 16:43:24 +0530

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Banner from '../components/Banner';
 
+// import SimNav from '../components/SimNav';
+// import Header from '../components/Header';
 const Home = () => {
     return (
        <>
